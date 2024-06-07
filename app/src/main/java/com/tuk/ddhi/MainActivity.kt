736ce27e951012f7f2ee87d047b3e9fe8e5899cb
fhddhdy37 <fhddhdy37@naver.com>
2024-06-07 10:27:56 +0900

    Add viewBinding
    Add viewBinding

--- app/src/main/java/com/tuk/ddhi/MainActivity.kt
+++ app/src/main/java/com/tuk/ddhi/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.tuk.ddhi
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.tuk.ddhi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_LOCATION_PERMISSION = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        requestLocationPermission()
+    }
+
+    private fun requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
         }
     }
 }
