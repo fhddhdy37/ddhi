304f6429b120c76c4207e19cde118fe016a5d313
GGeunit <165870924+GGeunit@users.noreply.github.com>
2024-06-21 16:36:02 +0900

    firebase + 액티비티
    액티비티(로그인, 회원가입 구성)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,4 @@
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -46,8 +47,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth.ktx)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
+    //implementation(libs.firebase.auth.ktx)
+    //implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
